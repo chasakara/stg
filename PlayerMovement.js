d4bd630ecd443af4c7b0f84b2c4bfe1906ec7f0a
Pegrume88 <pegrume88@gmail.com>
2023-05-12 12:59:25 +0000

    created variable for player position and functions for player moevement and position

---
+++ PlayerMovement.js
@@ -0,0 +1,490 @@
+let playerPosition = 0; // istarting point for the player on the game board.
+
+
+// Function to roll the dice and move the player(Ant)
+
+
+// Functions to move player
+function movePlayer(steps) {
+    playerPosition += steps; // Update players postiion depending on dice roll value
+}
+
+
+// function to handle player position
+function handlePlayerPosition(){
+    if (playerPosition === 0) {
+        // player is at starting position
+    }
+    else if (playerPosition === 1) {
+        // player is at starting position
+        // if first roll is one the player can choose to take a short cut
+    }
+    
+    else if (playerPosition === 2) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 3) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 4) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 5) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 6) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 7) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 8) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 9) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 10) {
+        // player is at starting position
+    }
+    else if (playerPosition === 11) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 12) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 13) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 14) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 15) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 16) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 17) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 18) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 19) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 20) {
+        // player is at starting position
+    }
+    else if (playerPosition === 22) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 23) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 24) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 25) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 26) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 27) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 28) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 29) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 30) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 31) {
+        // player is at starting position
+    }
+    else if (playerPosition === 32) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 33) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 34) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 35) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 36) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 37) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 38) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 39) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 40) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 41) {
+        // player is at starting position
+    }
+    else if (playerPosition === 42) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 43) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 44) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 45) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 46) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 47) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 48) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 49) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 50) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 51) {
+        // player is at starting position
+    }
+    else if (playerPosition === 52) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 53) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 54) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 55) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 56) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 57) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 58) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 59) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 60) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 61) {
+        // player is at starting position
+    }
+    else if (playerPosition === 62) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 63) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 64) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 65 {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 67) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 68) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 69) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 70) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 71) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 72) {
+        // player is at starting position
+    }
+    else if (playerPosition === 73) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 74) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 75) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 76) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 77) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 78) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 79) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 80) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 81) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 82) {
+        // player is at starting position
+    }
+    else if (playerPosition === 83) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 84) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 85) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 86) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 87) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 88) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 89) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 90) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 91) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 92) {
+        // player is at starting position
+    }
+    else if (playerPosition === 92) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 93) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 94) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 95) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 96) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 97) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 98) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 99) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 100) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 101) {
+        // player is at starting position
+    }
+    else if (playerPosition === 102) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 103) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 104) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 105) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 106) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 107) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 108) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 109) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 110) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 111) {
+        // player is at starting position
+    }
+    else if (playerPosition === 112) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 113) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 114) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 115) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 116) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 117) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 118) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 119) {
+        // player is at starting position
+    }
+    
+    else if (playerPosition === 120) {
+        // player is at starting position
+    }
+
+}
+
+
+// function for shortcuts
+
+
+// Function to calculate the next position based on the current position and number of steps
